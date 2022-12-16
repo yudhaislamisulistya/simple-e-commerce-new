7cbd7ce0ff818b1e412b73c3a2b44c015f83f084
hamdahanif <hizbullah.hamda@gmail.com>
2022-12-16 14:38:15 +0700

    Update AboutUs

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,11 @@
+import Header from '../components/header'
 export default function AboutUs() {
     return(
+        <>
+        <Header></Header>
         <div>
-        <h1>About Us yaaa</h1>
+        <h1>About Us </h1>
         </div>
+        </>
     )
 }
