6d199ee16203919a35d2f3a5ec5db49c41550114
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-12-16 15:21:00 +0700

    update conf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  future: {webpack5: true} ,
+  images: {
+      domains: ['images.ctfassets.net'],
+      disableStaticImages: true,
+      minimumCacheTTL: 60,
+  },
+  experimental: {
+      nextScriptWorkers: true,
+  },
 }
 
 module.exports = nextConfig
