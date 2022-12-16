8cc5f66fb3d1a45fd7d605bccc8edccc5b8ef20c
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-12-16 13:55:46 +0700

    update header and styles

---
+++ components/header.js
@@ -0,0 +1,27 @@
+import HeaderTitle from "./header/title";
+import MenuHeaderCenter from "./header/menu-header-center";
+import MenuHeaderRight from "./header/menu-header-right";
+
+const Header = () => {
+    return (
+        <header class="header">
+            <div class="header__inner fixed-header">
+                <div class="header__main">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="header__main-inner">
+                                    <HeaderTitle></HeaderTitle>
+                                    <MenuHeaderCenter></MenuHeaderCenter>
+                                    <MenuHeaderRight></MenuHeaderRight>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,12 @@
+import Header from "../components/header"
+
 export default function Home(){
     return(
+        <>
+        <Header></Header>
         <div>
             <h1>Home Mba Sandy Baru</h1>
         </div>
+        </>
     )
 }
